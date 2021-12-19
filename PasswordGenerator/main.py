db91f8cc55f635893d1742f3b5920e8354097dfc
Daria Sultanova <dariasultanova@gmail.com>
2021-12-19 20:18:25 +0300

    added generate_password()

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -11,13 +11,13 @@ exceptions = 'il1ILo0O'
 
 chars = ''
 
-pwd_quantity = int(input('Укажите количество паролей для генерации:'))
-pwd_length = int(input('Укажите длину одного пароля:'))
-pwd_digits = input('Включать ли цифры 0123456789? (да/нет)')
-pwd_uppercase = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (да/нет)')
-pwd_lowercase = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (да/нет)')
-pwd_punctuation = input('Включать ли символы !#$%&*+-=?@^_? (да/нет)')
-pwd_exceptions = input('Исключать ли неоднозначные символы il1ILo0O? (да/нет)')
+pwd_quantity = int(input('Укажите количество паролей для генерации: '))
+pwd_length = int(input('Укажите длину одного пароля: '))
+pwd_digits = input('Включать ли цифры 0123456789? (да/нет) ')
+pwd_uppercase = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (да/нет) ')
+pwd_lowercase = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (да/нет) ')
+pwd_punctuation = input('Включать ли символы !#$%&*+-=?@^_? (да/нет) ')
+pwd_exceptions = input('Исключать ли неоднозначные символы il1ILo0O? (да/нет) ')
 
 if pwd_digits == 'да':
     chars += digits
@@ -32,5 +32,9 @@ if pwd_exceptions == 'да':
         chars.replace(c, '')
 
 
-def generate_password(length, chars):
-    pass
+def generate_password(length=pwd_length, chars=chars):
+    for i in range(pwd_quantity):
+        print(''.join(random.sample(chars, length)))
+
+
+generate_password()
